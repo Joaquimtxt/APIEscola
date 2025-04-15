2c37e1ae8a060f10cc012cd049bf57b1bcfab26a
Joaquim <jocaq124@gmail.com>
2025-04-15 11:44:35 -0300

    Adicionado CORS, jwt Bearer, métodos, e IP como URL

--- APIEscola/Program.cs
+++ APIEscola/Program.cs
@@ -1,5 +1,7 @@
 using APIEscola.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,24 +10,82 @@ var builder = WebApplication.CreateBuilder(args);
 // Adicionar Servico de banco de dados
 builder.Services.AddDbContext<APIEscolaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Adicionando o serviço do CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+// Adicionar o serviço de autenticação
+// Serviço de EndPoints do Identity Framework
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = false; // Exige confirmação de email
+    options.SignIn.RequireConfirmedAccount = false; // Exige confirmação de conta
+    options.User.RequireUniqueEmail = true; // Exige email único
+    options.Password.RequireNonAlphanumeric = false; // Exige caracteres não alfanuméricos
+    options.Password.RequireLowercase = false; // Exige letras minúsculas
+    options.Password.RequireUppercase = false; // Exige letras maiúsculas
+    options.Password.RequireDigit = false; // Exige dígitos numéricos
+    options.Password.RequiredLength = 4; // Exige comprimento mínimo da senha
+})
+.AddRoles<IdentityRole>() // Adicionando o serviço de roles
+.AddEntityFrameworkStores<APIEscolaContext>() // Adicionando o serviço de EntityFramework
+.AddDefaultTokenProviders(); // Adiocionando o provedor de tokens padrão
+
+// Swagger com Autenticação JWT Bearer
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "APIEscola", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 builder.Services.AddControllers();
-//Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//Adicionar os Serviços de Autenticação e Autorização
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();//gerar o swagger
+
+app.UseSwaggerUI();//gerar a interface do swagger
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseCors("AllowAll");//configuração do CORS
 
-app.MapControllers();
+app.UseAuthentication();// Autenticação
+
+app.UseAuthorization();// Autorização
+
+app.MapControllers();// Mapear os controllers
+
+app.MapGroup("/Usuario").MapIdentityApi<IdentityUser>();// Mapear os endpoints de autenticação
 
 app.Run();
